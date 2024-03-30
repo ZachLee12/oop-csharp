b5ce45985d6cc6d8831cca3d40f63a8aa1a5511a
Zach Lee <leezhengyang22@gmail.com>
2024-03-30 20:16:01 +0100

    Finish ex4-5

--- MazeProject/Program.cs
+++ MazeProject/Program.cs
@@ -39,6 +39,43 @@
             //pla.move('E');
             //Console.WriteLine("The player is now in {0}", pla.getCurrentRoom().getName());
 
+            // Exerciser 4-5
+            Maze maze = new Maze();
+            Player player = new Player(maze.getStartRoom());
+            while(player.getCurrentRoom() != maze.getWinningRoom() && player.getCurrentRoom() != maze.getLosingRoom())
+            {
+                Console.WriteLine($"You are in {player.getCurrentRoom().getName()}");
+                Console.WriteLine("Type 'N', 'E', 'S', or 'W' to move to another room!");
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                while (keyInfo.KeyChar != 'N' && keyInfo.KeyChar != 'E' && keyInfo.KeyChar != 'S' && keyInfo.KeyChar != 'W')
+                {
+                    Console.WriteLine("Only 'N', 'E', 'S', or 'W'!");
+                    keyInfo = Console.ReadKey();
+                    Console.WriteLine("\n");
+                }
+
+                char direction = keyInfo.KeyChar;
+                Console.WriteLine("\n");
+                bool success = player.move(direction);
+                if (success)
+                {
+                    Console.WriteLine("You moved to another room!");
+                }
+                else
+                {
+                    Console.WriteLine("That didn't work!");
+                }
+
+                if(player.getCurrentRoom() == maze.getWinningRoom())
+                {
+                    Console.WriteLine("You Win! You are free!");
+                }
+                else if(player.getCurrentRoom() == maze.getLosingRoom())
+                {
+                    Console.WriteLine("You lose! You are dead!");
+                    break;
+                }
+            }
 
 
         }
